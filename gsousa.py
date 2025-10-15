61ac5a89918b1e859d1570a3f14b75bcccf7f026
Sousa1302 <sousaraposo1302@gmail.com>
2025-10-15 15:36:21 +0000

    Oraganization of ideas

---
+++ gsousa.py
@@ -0,0 +1,43 @@
+import time
+import pygame
+
+
+
+# Class Pou
+
+
+    # Constructor of Pou 
+
+    
+    # Update Stats - Hunger / Sleep / Hygiene / Energy / Happy  
+   
+
+    # Feed Function - Logical and change sprites  
+    # MiniGames Function - Logical and pop-up window with mini game
+    # Clean Function - Logical and change sprites 
+    # Sleep Function - Logical and change sprites
+    
+
+
+    # Pou's body - Upload sprites 
+
+        
+
+    # Get stats
+
+
+
+#Tasks: 
+    # Shop Function - Bonito
+
+    # Economy System - Costa 
+
+    # Save System - Ferreira 
+
+        
+    
+        
+
+
+    
+    
